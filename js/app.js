90a7d42476ee768d98d2b1764071b30625bef711
alfille <paul.alfille@gmail.com>
2022-07-08 22:58:55 -0400

    Clicking in the garden works. Garden history works Needs back Needs hover

--- js/app.js
+++ js/app.js
@@ -127,38 +127,60 @@ var G = new Game();
 class GardenView {
     constructor() {
         this.svg = document.getElementById("svg");
-        this.show() ;
     }
 
-    create_svg() {
-        let f = G.foxes;
-        let Th = f.map( (_,i) => `<use href="#svg_hole" transform="rotate(${360.*i/f.length})" />`).join(" ");
-        let Tf = f.map( (ff,i) => `<use href=${ff?"#svg_fox":"#svg_nofox"} transform="rotate(${360.*i/f.length})" />`).join(" ");
-        let Tl = f.map( (ff,i) => `<use href=${ff?"#svg_larrow":"#svg_nofox"} transform="rotate(${360.*i/f.length})" />`).join(" ");
-        let Tr = f.map( (ff,i) => `<use href=${ff?"#svg_rarrow":"#svg_nofox"} transform="rotate(${360.*i/f.length})" />`).join(" ");
-        return `
-        <svg viewBox="-1000 -1000 2000 2000">
-            <rect width="1000" height="1000" style="fill:rgb(0,0,255);" />  
+    start() {
+        this.history = [] ;
+    }
+
+    back() {
+        this.history.pop();
+    }
+
+    add_history_row(s) {
+        let Th = s.map( (_,i) => `<g id=${"bottom_"+i}><use href="#old_hole" transform="rotate(${360.*i/s.length})" /></g>`).join("");
+        let Tf = s.map( (ss,i) => `<g transform="rotate(${360.*i/s.length})"><text class="old_fox" x="42" y="580" rotate="180">${ss}</text></g>`).join("");
+        this.history.push( `<circle cx="0" cy="0" r="600" stroke="grey" stroke-width="3" fill="none" />
+            <def><circle id="old_hole" cx="0" cy="600" r="50" /></def>
+            ${Th}${Tf}`);
+    }
+        
+    show_history() {
+        if ( this.history.length == 0 ) {
+            return "";
+        }
+        return this.history.reduce( (t,x) => `<g transform="scale(.83)">${t}</g>${x}` );
+    }
+
+    create_svg(s) {
+        let f = G.foxes ;
+        let Th = s.map( (_,i) => `<g id=${"bottom_"+i}><use href="#svg_hole" transform="rotate(${360.*i/s.length})" /></g>`).join("");
+        let Tf = s.map( (ss,i) => `<g transform="rotate(${360.*i/s.length})"><text class="svg_fox" x="125" y="740" rotate="180">${ss}</text></g>`).join("");
+        let Tl = f.map( (ff,i) => `<use href=${ff?"#svg_larrow":"#svg_nofox"} transform="rotate(${360.*i/f.length})" />`).join("");
+        let Tr = f.map( (ff,i) => `<use href=${ff?"#svg_rarrow":"#svg_nofox"} transform="rotate(${360.*i/f.length})" />`).join("");
+        let Tc = s.map( (_,i) => `<use href="#svg_click" data-hole=${i+""} id=${"top_"+i} transform="rotate(${360.*i/s.length})" />`).join("");
+        return `<svg viewBox="-1000 -1000 2000 2000">
             <circle cx="0" cy="0" r="803" stroke="grey" stroke-width="3" fill="none" />
             <circle cx="0" cy="0" r="797" stroke="grey" stroke-width="3" fill="none" />
             <def>
-                <circle id="svg_hole" cx="0" cy="800" r="150" stroke="black" stroke-width="10" fill="tan" />
-                <text id="svg_fox" x="125" y="740" fill="black" font-size="200" rotate="180">&#129418;</text>
-                <text id="svg_nofox" x="125" y="740" fill="black" font-size="200" rotate="180">&nbsp;</text>
-                <text id="svg_rarrow" x="100" y="790" font-size="200" fill="red" rotate="-15">&#8594;</text>
-                <text id="svg_larrow" x="-300" y="740" font-size="200" fill="red" rotate="15">&#8592;</text>
+                <circle id="svg_hole" cx="0" cy="800" r="150" />
+                <text id="svg_rarrow" x="100" y="790" rotate="-15">&#8594;</text>
+                <text id="svg_larrow" x="-300" y="740" rotate="15">&#8592;</text>
+                <circle id="svg_click" cx="0" cy="800" r="150" />
             </def>
             ${Th}
             ${Tf}
             ${Tl}
             ${Tr}
+            ${Tc}
+            ${this.show_history()}
             Sorry, your browser does not support inline SVG.  
-        </svg>
-        ` ;
+        </svg>` ;
     }
 
-    show() {
-        this.svg.innerHTML = this.create_svg() ;
+    control_row(symbol_list) {
+        this.svg.innerHTML = this.create_svg(symbol_list) ;
+        this.svg.onload = G.foxes.forEach( (_,i)=>document.getElementById("top_"+i).addEventListener('click', (e) => T.click(e.target.id.split('_')[1]) ));
     }
 }
 var GV = new GardenView();
@@ -199,11 +221,18 @@ class Table {
     start() {
         this.header();
         G.start();
+        GV.start();
         this.tbody.innerHTML = "";
         this.control_row();
         this.update();
     }
     
+    click(hole) {
+        [...this.tbody.lastElementChild
+            .querySelectorAll("input")]
+            .filter( i=>parseInt(i.getAttribute("data-n"))==hole )[0].click();
+    }
+
     check(hole) {
         let h = [...this.tbody.lastElementChild
             .querySelectorAll("input")]
@@ -217,6 +246,7 @@ class Table {
     control_row() {
         let f = G.foxes ;
         let r = document.createElement("tr");
+        let s = this.symbols( [], [], f );
         for ( let i = 0; i <= H.value ; ++i ) {
             let d = document.createElement("td");
             if ( i==0 ) {
@@ -225,7 +255,7 @@ class Table {
                 b.onclick = () => T.back() ;
                 d.appendChild(b);
             } else {
-                d.innerHTML = (f[i-1] ? "&#129418" : "&nbsp;") + "<br>" ;
+                d.innerHTML = s[i-1] + "<br>" ;
                 let b = document.createElement("input");
                 b.type = "checkbox";
                 b.onclick = () => T.check(i-1) ;
@@ -236,7 +266,7 @@ class Table {
         }
         let d = document.createElement("td");
         this.tbody.appendChild(r);
-        GV.show();
+        GV.control_row(s);
     }
 
     back() {
@@ -263,7 +293,7 @@ class Table {
     move(holes) { // hole 0-based
         G.move(holes);
         this.remove_row();
-        this.static_row();
+        this.add_history_row();
         if ( G.number == 0 ) {
             this.victory_row();
         } else {
@@ -272,28 +302,33 @@ class Table {
         this.update();
     }
 
-    static_row() {
+    symbols( moves, poisons, foxes ) {
+        // moves = list of inspection holes
+        // poisons = list of poisoned holes
+        // foxes = true/false fox occupation list
+        // returns a symbol list
+        let s = foxes.map( f => f?"&#129418;":"&nbsp;" ) ;
+        moves.forEach( m => s[m] = "&#x274c;" );
+        poisons.forEach( p => s[p] = "&#9760;" );
+        return s ;
+    } 
+
+    add_history_row() { // historical row
         let [m,p,f] = G.prior ;
-        console.log(G.Prior);
         let r = document.createElement("tr");
+        let s = this.symbols( m, p, f );
         for ( let i = 0; i <= H.value ; ++i ) {
-            let h = i-1; // actual hole index after first column
             let d = document.createElement("td");
             if ( i==0 ) {
                 d.innerHTML = `Day ${G.day-1}`;
-            } else if ( m.indexOf(h) > -1 ) {
-                d.innerHTML = "&#x274c" ; //X
-            } else if ( p.indexOf(h) > -1 ) {
-                d.innerHTML = "&#9760" ; //Poison
-            } else if (f[h]) {
-                d.innerHTML = "&#129418" ; //fox
             } else {
-                d.innerHTML = "&nbsp;" ; //empty
+                d.innerHTML = s[i-1] ;
             }
             r.appendChild(d);
         }
         let d = document.createElement("td");
         this.tbody.appendChild(r);
+        GV.add_history_row(s);
     }
 
     remove_row() {
@@ -318,11 +353,7 @@ class Table {
     }
     
 }
-var T = new Table();
-
-
-
-             
+var T = new Table();             
 
 function changeInput() {
     if ( H.change ) {
@@ -348,10 +379,6 @@ class Overlay {
 	}
 }
 var O = new Overlay();
-			
-		
-		
-
 
 // Application starting point
 window.onload = () => {
